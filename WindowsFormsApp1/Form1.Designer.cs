18749a6d50e558d868b730495103cf27bc86a9e7
muhammad akbar yasin <akbar.yasin@m2.formulatrix.com>
2019-09-20 19:32:19 +0700

    Coba
    Signed-off-by: muhammad akbar yasin <m.akbar.yasin@gmail.com>

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_gas = new System.Windows.Forms.Button();
+            this.btn_rem = new System.Windows.Forms.Button();
+            this.btn_indicator = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_gas
+            // 
+            this.btn_gas.Location = new System.Drawing.Point(98, 12);
+            this.btn_gas.Name = "btn_gas";
+            this.btn_gas.Size = new System.Drawing.Size(75, 23);
+            this.btn_gas.TabIndex = 0;
+            this.btn_gas.Text = "Gas!";
+            this.btn_gas.UseVisualStyleBackColor = true;
+            this.btn_gas.Click += new System.EventHandler(this.btn_gas_Click);
+            // 
+            // btn_rem
+            // 
+            this.btn_rem.Location = new System.Drawing.Point(98, 41);
+            this.btn_rem.Name = "btn_rem";
+            this.btn_rem.Size = new System.Drawing.Size(75, 23);
+            this.btn_rem.TabIndex = 1;
+            this.btn_rem.Text = "Rem!";
+            this.btn_rem.UseVisualStyleBackColor = true;
+            this.btn_rem.Click += new System.EventHandler(this.btn_rem_Click);
+            // 
+            // btn_indicator
+            // 
+            this.btn_indicator.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_indicator.Location = new System.Drawing.Point(98, 70);
+            this.btn_indicator.Name = "btn_indicator";
+            this.btn_indicator.Size = new System.Drawing.Size(75, 23);
+            this.btn_indicator.TabIndex = 2;
+            this.btn_indicator.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 109);
+            this.Controls.Add(this.btn_indicator);
+            this.Controls.Add(this.btn_rem);
+            this.Controls.Add(this.btn_gas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_gas;
+        private System.Windows.Forms.Button btn_rem;
+        private System.Windows.Forms.Button btn_indicator;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Car MyCar;
+        public Form1()
+        {
+
+            InitializeComponent();
+            MyCar = new Car(20);
+            MyCar.OnVariableChange += MyCar_OnVariableChange;
+        }
+
+        private void MyCar_OnVariableChange(int newVal)
+        {
+            Console.WriteLine(newVal);
+            RefreshIndicator();
+        }
+
+        private void btn_gas_Click(object sender, EventArgs e)
+        {
+            MyCar.NgeGas();
+        }
+
+        private void btn_rem_Click(object sender, EventArgs e)
+        {
+            MyCar.NgeRem();
+        }
+
+        private void RefreshIndicator()
+        {
+            if (!MyCar.isSafeSpeed())
+            {
+                btn_indicator.BackColor = Color.Red;
+                MessageBox.Show("Bahaya cuy, jangan terlalu laju.", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (MyCar.getVelocity() == 0)
+                btn_indicator.BackColor = Color.Transparent;
+            else btn_indicator.BackColor = Color.Green;
+        }
+    }
+
+    class Car
+    {
+        private int Velocity;
+        private int MaxVelocity;
+        // public enum ColorIndicator { Red, Green, None };
+
+        public delegate void OnVariableChangeDelegate(int newVal);
+        public event OnVariableChangeDelegate OnVariableChange;
+
+        public Car(int maxVelocity)
+        {
+            Velocity = 0;
+            MaxVelocity = maxVelocity;
+        }
+
+        public int getVelocity()
+        {
+            return Velocity;
+        }
+
+        public void setVelocity(int velocity)
+        {
+            if (velocity == Velocity) return;
+
+            Velocity = velocity;
+
+            OnVariableChange?.Invoke(velocity);
+
+            // if (OnVariableChange != null)
+            //    OnVariableChange(velocity);
+        }
+
+        public void NgeGas()
+        {
+            Velocity = Velocity + 5;
+            setVelocity(getVelocity() + 5);
+        }
+
+        public void NgeRem()
+        {
+            int currentVelocity = getVelocity();
+
+            if (currentVelocity > 0 )
+                setVelocity(currentVelocity - 5);
+            
+            // if (currentVelocity < 0)
+            //     setVelocity(0);
+        }
+
+        public bool isSafeSpeed()
+        {
+            if (Velocity <= MaxVelocity)
+                return true;
+            else return false;
+        }
+
+        /*public ColorIndicator getColorIndicator()
+        {
+            if (Velocity <= MaxVelocity)
+                return ColorIndicator.Green;
+            else if (Velocity == 0)
+                return ColorIndicator.None;
+            else return ColorIndicator.Red;
+        }*/
+    }
+}
